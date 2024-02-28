bb7cb63b4762c4ddaf559f9457c5642385b8c0b0
“bpa812” <braxton.arentz@outlook.com>
2024-02-28 15:20:09 +1100

    basic game mechs
    modding sdk soon (mod codes)

---
+++ lifesim.py
@@ -0,0 +1,25 @@
+import random
+#The following work is a life simulator made by Github user BPA812
+print("Welcome to PyLife, to start please answer the questions below.")
+d1 = 'N'
+prefixes = ['Tok','Del','Shang','São','Mexico','Cai','Mum','Bei','Dh','Os','New York','Kara','Buenos','Chong','Istan']
+suffixes = ['yo','hi','hai',' Paulo',' City','ro','bai','jing','aka','chi',' Aires','qing','bul']
+prefixes2 = ['Chi','Ind','United','Indon','Paki','Nige','Bra','Bangla','Rus','Mex','Jap','Philipp','Ethio','Egy','Viet']
+suffixes2 = ['na','ia',' States','esia','stan','ria','zil','desh','sia','ico','an','ines','pia','pt','nam']
+prefixes3 = ['Ala','Al','Am','But','Cala','Col','Contra','Del','El','Fres','Gl','Hum','Imp','In','Ke','Ki','La','Las','Los','Mad','Ma','Mari','Mendo','Mer','Mo','Mo','Mont','Na','Nev','Ora','Pla','Plu','River','Sacra','San','San','San','San','San','San','San','Santa','Santa','Santa','Sha','Sier','Sisk','Sol','Son','Stani','Sut','Teh','Trin','Tul','Tuol','Ven','Yo','Yu']
+suffixes3 = ['meda','pine','ador','te','veras','usa',' Costa',' Norte',' Dorado','no','enn','boldt','erial','yo','rn','ngs','ke','sen',' Angeles','era','rin','posa','cino','ced','doc','no','erey','pa','ada','nge','cer','mas','side','mento',' Benito',' Bernardino',' Diego',' Francisco',' Joaquin',' Luis Obispo',' Mateo',' Barbara',' Clara', ' Cruz','sta','ra','iyou','ano','oma','slaus','ter','ama','ity','are','umne','tura','lo','ba']
+def generatecity():
+    return prefixes[random.randint(0,len(prefixes))] + suffixes[random.randint(0,len(suffixes))]
+def generatecountry():
+    return prefixes2[random.randint(0,len(prefixes))] + suffixes2[random.randint(0,len(suffixes))]
+def generatecounty():
+    return prefixes3[random.randint(0,len(prefixes))] + suffixes3[random.randint(0,len(suffixes))]
+#Make Character
+while d1 == 'N':
+    fname = input("Character First Name:")
+    lname = input("Character Last Name:")
+    gender = input("Character Gender:")
+    d1 = input("Are you sure about your character's info? (Y/N)")
+print(generatecity())
+print(generatecountry())
+print(generatecounty())
